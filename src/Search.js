985abf086255a46485799698bc5056b8964b02bc
rohitsanjay <rohitsanjay64@gmail.com>
2019-11-13 22:49:27 +0530

    added functionalities for filters

--- src/Search.js
+++ src/Search.js
@@ -1,6 +1,24 @@
 import React, { Component } from "react";
 import "./Search.css";
 
+function arrUnique(arr) {
+  var cleaned = [];
+  arr.forEach(function(itm) {
+      var unique = true;
+      cleaned.forEach(function(itm2) {
+          if(itm.name.value === itm2.name.value)
+          {
+            unique = false;
+          }
+      });
+      if(unique)
+      {
+        cleaned.push(itm);
+      }
+  });
+  return cleaned;
+}
+
 class Search extends Component {
   state = {
     searchValue: "",
@@ -18,27 +36,40 @@ class Search extends Component {
 
   handleSearchTour = () => {
     this.makeApiCall("tour", this.state.searchValue);
-  }
+  };
 
   makeApiCall = (type, searchInput) => {
     var searchUrl;
-    if (type === "food") {
-    searchUrl = `https://www.themealdb.com/api/json/v1/1/search.php?s=${searchInput}`;}
-    else { searchUrl = `https://dbpedia.org/sparql?default-graph-uri=http%3A%2F%2Fdbpedia.org&query=PREFIX+dbo%3A+%3Chttp%3A%2F%2Fdbpedia.org%2Fontology%2F%3E%0D%0APREFIX+dbp%3A+%3Chttp%3A%2F%2Fdbpedia.org%2Fresource%2F%3E%0D%0APREFIX+foaf%3A+%3Chttp%3A%2F%2Fxmlns.com%2Ffoaf%2F0.1%2F%3E%0D%0A%0D%0ASELECT+%3Fname+%3Fimage++where+%7B%0D%0A++++%3Falpha+dct%3Asubject+dbc%3A${searchInput}.%0D%0A++++%3Falpha+rdfs%3Alabel+%3Fname+.%0D%0A++++%3Falpha+foaf%3Adepiction+%3Fimage%0D%0A++++FILTER+%28lang%28%3Fname%29+%3D+%27en%27%29%0D%0A%0D%0A++++%0D%0A%0D%0A%7D&format=application%2Fsparql-results%2Bjson&CXML_redir_for_subjs=121&CXML_redir_for_hrefs=&timeout=30000&debug=on&run=+Run+Query+`}
+    if (type === "food")
+    {
+      searchUrl = `https://dbpedia.org/sparql?default-graph-uri=http%3A%2F%2Fdbpedia.org&query=PREFIX+dbo%3A+%3Chttp%3A%2F%2Fdbpedia.org%2Fontology%2F%3E%0D%0APREFIX+dbp%3A+%3Chttp%3A%2F%2Fdbpedia.org%2Fresource%2F%3E%0D%0APREFIX+foaf%3A+%3Chttp%3A%2F%2Fxmlns.com%2Ffoaf%2F0.1%2F%3E%0D%0A%0D%0ASELECT+%3Fname+%3Fimage++where+%7B%0D%0A++++%3Falpha+dct%3Asubject+dbc%3A${searchInput}.%0D%0A++++%3Falpha+rdfs%3Alabel+%3Fname+.%0D%0A++++%3Falpha+foaf%3Adepiction+%3Fimage%0D%0A++++FILTER+%28lang%28%3Fname%29+%3D+%27en%27%29%0D%0A%0D%0A++++%0D%0A%0D%0A%7D&format=application%2Fsparql-results%2Bjson&CXML_redir_for_subjs=121&CXML_redir_for_hrefs=&timeout=30000&debug=on&run=+Run+Query+`;
+    }
+    else
+    {
+      searchUrl = `https://dbpedia.org/sparql?default-graph-uri=http%3A%2F%2Fdbpedia.org&query=PREFIX+dbo%3A+%3Chttp%3A%2F%2Fdbpedia.org%2Fontology%2F%3E%0D%0APREFIX+dbp%3A+%3Chttp%3A%2F%2Fdbpedia.org%2Fresource%2F%3E%0D%0APREFIX+foaf%3A+%3Chttp%3A%2F%2Fxmlns.com%2Ffoaf%2F0.1%2F%3E%0D%0A%0D%0ASELECT++%3Fname+%3Fimage+where+%7B%0D%0A+++%3Fbeta+++skos%3Abroader+dbc%3ATourist_attractions_by_country.%0D%0A++++%3Fgamma++skos%3Abroader+%3Fbeta+.%0D%0A+++%3Fdelta++dct%3Asubject+%3Fgamma++.%0D%0A++++%3Fdelta+dbo%3Alocation+dbr%3A${searchInput}+.%0D%0A+++%3Fdelta+foaf%3Aname+%3Fname+.%0D%0A++OPTIONAL+%7B+%3Fdelta+foaf%3Adepiction+%3Fimage+%7D%0D%0A+++%0D%0A+++FILTER+%28lang%28%3Fname%29+%3D+%27en%27%29%0D%0A++++%0D%0A+++%0D%0A%0D%0A%7D&format=application%2Fsparql-results%2Bjson&CXML_redir_for_subjs=121&CXML_redir_for_hrefs=&timeout=30000&debug=on&run=+Run+Query+`;
+      //searchUrl = `https://dbpedia.org/sparql?default-graph-uri=http%3A%2F%2Fdbpedia.org&query=PREFIX+dbo%3A+%3Chttp%3A%2F%2Fdbpedia.org%2Fontology%2F%3E%0D%0APREFIX+dbp%3A+%3Chttp%3A%2F%2Fdbpedia.org%2Fresource%2F%3E%0D%0APREFIX+foaf%3A+%3Chttp%3A%2F%2Fxmlns.com%2Ffoaf%2F0.1%2F%3E%0D%0A%0D%0ASELECT+%3Fdelta+%3Fbeta+%3Fgamma+%3Fname+where+%7B%0D%0A+++%3Fbeta+++skos%3Abroader+dbc%3ATourist_attractions_by_country++.%0D%0A++++%3Fgamma++skos%3Abroader+%3Fbeta+.%0D%0A+++%3Fdelta++dct%3Asubject+%3Fgamma++.%0D%0A++++%3Fdelta+dbo%3Alocation+dbr%3A${searchInput}+.%0D%0A+++%3Fdelta+foaf%3Aname+%3Fname%0D%0A+++FILTER+%28lang%28%3Fname%29+%3D+%27en%27%29%0D%0A++++%0D%0A+++%0D%0A%0D%0A%7D&format=application%2Fsparql-results%2Bjson&CXML_redir_for_subjs=121&CXML_redir_for_hrefs=&timeout=30000&debug=on&run=+Run+Query+`;
+    }
     fetch(searchUrl)
       .then(response => {
         return response.json();
       })
       .then(jsonData => {
         if (type === "food") {
-          this.state.places ? (delete this.state.places) : console.log("meals deleted");
-         this.setState({ cuisines: jsonData.meals });
-         delete this.state.meals;
+          this.setState({ cuisines: jsonData.results.bindings });
+          if(this.state.place)
+          {
+            delete this.state.place;
+            console.log('Done deleting the fucking places!!!!');
+          }
         }
-        else {
-          this.state.cuisines ? (delete this.state.cuisines) : console.log("places deleted");
-          this.setState({ places: jsonData.results.bindings });
-          delete this.state.meals;
+        else
+        {
+          if(this.state.cuisines)
+          {
+            delete this.state.cuisines;
+            console.log('Done deleting the fucking cuisines!!!!!');
+          }
+          this.setState({ places: arrUnique(jsonData.results.bindings) });
         }
         console.log(this.state);
       });
@@ -47,7 +78,7 @@ class Search extends Component {
   render(){
     return (
       <div id="main">
-        <h1>Welcome</h1>
+        <h1>Welcome bastards!</h1>
         <input
           name="text"
           type="text"
@@ -60,9 +91,9 @@ class Search extends Component {
         {this.state.cuisines ? (
           <div id="cuisine-container">
             {this.state.cuisines.map((cuisine, index) => (
-              <div class="single-cuisine" key={index}>
-                <h2>{cuisine.strMeal}</h2>
-                <img src={cuisine.strMealThumb} alt="cuisine thumbnail not available!" />
+              <div className="single-cuisine" key={index}>
+                <h2>{cuisine.name.value}</h2>
+                <img src={cuisine.image.value} alt="Oh jeez not available!" />
               </div>
             ))}
           </div>
@@ -72,7 +103,7 @@ class Search extends Component {
         {this.state.places ? (
           <div id="cuisine-container">
             {this.state.places.map((place, index) => (
-              <div class="single-cuisine" key={index}>
+              <div className="single-cuisine" key={index}>
                 <h2>{place.name.value}</h2>
                 <img src={place.image.value} alt="place thumbnail not available!" />
               </div>
@@ -81,7 +112,7 @@ class Search extends Component {
         ) : (
           <p></p>
         )}
-        {(!this.state.places && !this.state.cuisines) ? (<p>You haven't searched anything!</p>) : (<p></p>)};
+        {(!this.state.places && !this.state.cuisines) ? (<p>You haven't searched anything!</p>) : (<p></p>)}
       </div>
     );
   }
